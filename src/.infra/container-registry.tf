b4ceb1663013b534bb53f98fb2eded74c5175298
Josh Rolstad <jrolstad@gmail.com>
2022-01-22 08:48:17 -0800

    Adding container registry

--- src/.infra/container-registry.tf
+++ src/.infra/container-registry.tf
@@ -1,5 +1,14 @@
-resource "aws_ecr_repository" "registry" {
+resource "aws_ecr_repository" "registry-frontend" {
   name                 = "rolstadlearnk8s-frontend-static"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "registry-api" {
+  name                 = "rolstadlearnk8s-frontend-api"
   image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
